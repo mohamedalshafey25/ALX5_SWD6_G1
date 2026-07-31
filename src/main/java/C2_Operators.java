175e6f2efbd32178f5f6c9d36fba6626e0eecac8
mohamedalshafey25 <52078877+mohamedalshafey25@users.noreply.github.com>
2026-07-31 10:26:19 +0300

    Session 2: Operators.

---
+++ src/main/java/C2_Operators.java
@@ -0,0 +1,27 @@
+public class C2_Operators {
+
+    public static void main(String[] args) {
+        // Operators in Java
+        /**
+         * Arithmetic Operators: +, -, *, /, %
+         * Unary Operators: +, -, ++, --
+         * Relational Operators: ==, !=, >, <, >=, <=
+         * Logical Operators: &&, ||, !
+         * Assignment Operators: =, +=, -=, *=, /=, %=
+         */
+
+        // Arithmetic Operators
+        int a = 10;
+        int b = 3;
+        System.out.println("a + b = " + (a + b)); // 13
+        System.out.println("a - b = " + (a - b)); // 7
+        System.out.println("a * b = " + (a * b)); // 30
+        System.out.println("a / b = " + (a / b)); // 3
+        System.out.println("a % b = " + (a % b)); // 1
+
+
+
+
+
+    }
+}
